29fec3a8bc26f3c4fbcd161ca82faa3954d0e2fc
S_ghanbary98 <67980207+Sherv1360@users.noreply.github.com>
2021-06-24 10:03:14 +0100

    first commit

---
+++ python_functions.py
@@ -0,0 +1,7 @@
+# Creating Functions
+## Syntax
+### The syntax used to declare a function is `def` then the name of the function():.
+
+
+def greeting():
+    print("Welcome on Board! enjoy your trip")
